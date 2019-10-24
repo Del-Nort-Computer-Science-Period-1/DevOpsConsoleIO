22adb58e3da8208c3180c9a2c5bb5af3ec8b7749
Colin Szeto <kolinszeto@gmail.com>
2019-10-24 08:43:35 -0700

    Ready For operation

---
+++ DevOpsConsoleIO/DevOpsConsoleIO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DevOpsConsoleIO
+//
+//  Created by Szeto, Colin on 10/24/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+// Colin was here
